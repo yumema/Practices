0ab7273430a7fe55253d7b90291d2f770f5f7795
Marina <yumema@gmail.com>
2013-01-15 09:59:12 -0800

    Updated usage and structure
    Updated Usage and added beginning structure for handling options

--- Pcompress/main.pl
+++ Pcompress/main.pl
@@ -6,26 +6,42 @@ use Getopt::Std;
 sub usage;
 
 my %options = ();
-my $chosen = getopt("c:d:o");
-if ( !getopts("c:d:o:", \%options) )
+if ( !getopts("c:d:o:h", \%options) )
 {
   usage;
 }
 else
 {
   # temporary placeholder until implementation complete 
-  print "Option was used\n"; 
+  if (defined $options{c})
+  {
+    print "Compressing $options{c} ";
+  }
+  elsif (defined $options{d})
+  {
+    print "Decompressing $options{d} "; 
+  }
+  elsif (defined $options{h})
+  {
+    usage;
+  }
+  else
+  {
+    print "Missing argument or option. Use -h for help\n"
+  }
 }
 
 
 
 sub usage(){
-  print "Pcompress usage:\n\n";
-  print "pcompress -[cd] file [-o output file] \n\n";
+  print "\nPCOMPRESS USAGE\n\n";
+  print "\tpcompress -[c|d] file [-o outputFile] \n";
+  print "\tpcompress -h\n\n";
   print "DESCRIPTION\n";
-  print "pcompress uses huffman coding to compress/decompress a given file\n\n";
+  print "\tpcompress uses huffman coding to compress/decompress a given file\n\n";
   print "OPTIONS\n";
-  print "-c\tCompress the file\n";
-  print "-d\tDecompress the file\n";
-  print "-o\tOutput the compressed/decompressed file to the named output file\n\n";
+  print "\t-c\tCompress the file\n";
+  print "\t-d\tDecompress the file\n";
+  print "\t-o\tOutput the compressed/decompressed file to the named output file\n";
+  print "\t-h\tPrints Pcompress usage\n\n"
  } 
